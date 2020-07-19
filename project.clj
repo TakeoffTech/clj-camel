c243880aeabbdc41df5fa7a5228e2fb679d3fa4b
Taras S <tarassuhovenko@gmail.com>
2020-07-19 14:21:45 +0300

    updated license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject takeoff/clj-camel "1.0.0"
+(defproject takeoff/clj-camel "2020-07-20.3.4.1"
   :description "Clojure wrapper for Apache Camel"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/TakeoffTech/clj-camel"
+  :license {:name "Apache License Version 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.0.0"]
 
