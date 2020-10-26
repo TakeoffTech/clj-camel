fdcb18cc05ca468eb25016d34a289276814dadeb
Taras <tarassuhovenko@gmail.com>
2020-10-26 10:22:41 +0200

    added camel file headers mapping (#16)

--- src/main/clj/clj_camel/headers.clj
+++ src/main/clj/clj_camel/headers.clj
@@ -17,5 +17,20 @@
            :camel-split-complete     "CamelSplitComplete"
            :camel-split-size         "CamelSplitSize"
 
+           :camel-file-absolute      "CamelFileAbsolute"
+           :camel-file-absolute-path "CamelFileAbsolutePath"
+           :camel-file-host          "CamelFileHost"
+           :camel-file-last-modified "CamelFileLastModified"
+           :camel-file-length        "CamelFileLength"
+           :camel-file-name          "CamelFileName"
+           :camel-file-name-consumed "CamelFileNameConsumed"
+           :camel-file-name-only     "CamelFileNameOnly"
+           :camel-file-name-produced "CamelFileNameProduced"
+           :camel-file-parent        "CamelFileParent"
+           :camel-file-path          "CamelFilePath"
+           :camel-file-relative-path "CamelFileRelativePath"
+           :camel-ftp-reply-code     "CamelFtpReplyCode"
+           :camel-ftp-reply-string   "CamelFtpReplyString"
+
            :x-token                  "x-token"
            :x-service-origin         "x-service-origin"})
