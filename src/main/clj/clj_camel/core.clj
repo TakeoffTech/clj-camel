7cebb0df373416c7f7c007a145956eebd4b745c8
Denys Klymenko <gameforgloryggwp@gmail.com>
2021-04-15 12:59:33 +0300

    Aggregation completion on batch consumer & rollback camel 3.9.0 (#31)
    * Aggregation completion on batch consumer & rollback camel 3.9.0
    
    * Eager check completion

--- src/main/clj/clj_camel/core.clj
+++ src/main/clj/clj_camel/core.clj
@@ -370,6 +370,8 @@
   (c/aggregate (c/constant 1) c/grouped-body-strategy
                {:completion-size      1000
                 :completion-timeout   1000
+                :completion-from-batch-consumer true
+                :eager-check-completion true
                 :completion-predicate (c/predicate (fn [_] true))})
   (c/to 'direct:result')
   ...
@@ -378,11 +380,15 @@
   (let [{:keys [completion-size
                 completion-timeout
                 parallel-processing
+                completion-from-batch-consumer
+                eager-check-completion
                 completion-predicate]} opts]
     `(-> (.aggregate ~pd ~expression ~strategy)
          ~@(core-when (some? completion-size) `((.completionSize ~completion-size)))
          ~@(core-when (some? completion-timeout) `((.completionTimeout ~completion-timeout)))
          ~@(core-when (some? parallel-processing) `((.parallelProcessing ~parallel-processing)))
+         ~@(core-when (some? completion-from-batch-consumer) `((.completionFromBatchConsumer)))
+         ~@(core-when (some? eager-check-completion) `((.eagerCheckCompletion)))
          ~@(core-when (some? completion-predicate) `((.completionPredicate ~completion-predicate)))
          ~@(concat body `((.end))))))
 
