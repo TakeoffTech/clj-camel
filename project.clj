5de8ed5d2d90df1ebd9107cb669747913d94a7cf
Vitalii Grodskyi <vitaliy.grodskiy@takeoff.com>
2021-08-12 15:02:05 +0300

    [INC-116] Update to camel 3.11.0 (#37)
    * [INC-116] Update to camel 3.11.0
    
    * set version to 2.0.0
    
    * updated changelog and readme with resolved issues during camel migration

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject takeoff/clj-camel "latest"
+(def camel-version "3.11.0")
+
+(defproject takeoff/clj-camel "2.0.0"
   :description "Clojure wrapper for Apache Camel"
   :url "https://github.com/TakeoffTech/clj-camel"
   :license {:name "Apache License Version 2.0"
@@ -6,11 +8,11 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.0.0"]
 
-                 [org.apache.camel/camel-core "3.8.0"]
-                 [org.apache.camel/camel-sql "3.8.0"]
-                 [org.apache.camel/camel-jcache "3.8.0"]
-                 [org.apache.camel/camel-management "3.8.0"]
-                 [org.apache.camel/camel-ftp "3.8.0"]
+                 [org.apache.camel/camel-core       ~camel-version]
+                 [org.apache.camel/camel-sql        ~camel-version]
+                 [org.apache.camel/camel-jcache     ~camel-version]
+                 [org.apache.camel/camel-management ~camel-version]
+                 [org.apache.camel/camel-ftp        ~camel-version]
                  [malabarba/lazy-map "1.3"]
 
                  [camel-snake-kebab "0.4.1"]
@@ -29,6 +31,7 @@
 
   :profiles {:test    {:dependencies [[com.rpl/specter "1.1.3"]
                                       [org.ehcache/ehcache "3.8.1"]
-                                      [org.apache.camel/camel-http "3.8.0"]
-                                      [org.apache.camel/camel-jsonpath "3.8.0"]]}
+                                      [org.apache.camel/camel-http     ~camel-version]
+                                      [org.apache.camel/camel-jsonpath ~camel-version]
+                                      [ch.qos.logback/logback-classic "1.2.3"]]}
              :uberjar {:aot :all}})
